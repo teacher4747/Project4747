c5485d1f4f730ec36ba1048a2377c88109443a25
teacher4747 <ucitelnicagimnazia47@gmail.com>
2025-10-22 16:29:38 +0500

    homework.4lessons.14/10/2025

--- Project4747.cpp
+++ Project4747.cpp
@@ -8,7 +8,7 @@
 int main()
 {
 	setlocale(LC_ALL, "RUS");
-	std::cout << "Здравствуйте, введите расстояние до аэропорта (в км) и время, за которое нужно доехать (в часах) \n";
+	/*std::cout << "Здравствуйте, введите расстояние до аэропорта (в км) и время, за которое нужно доехать (в часах) \n";
 	double distance = 0;
 	std::cin >> distance;
 	double time = 0;
@@ -49,9 +49,9 @@ int main()
 	double final_sum = duration_minutes * tariff_per_minute;
 
 	std::cout << "С вас " << final_sum << " гривен";
-
+*/
 	using namespace std;
-
+	/*
 	double distance, fuelConsumption;
 	double price1, price2, price3;
 
@@ -105,14 +105,76 @@ int main()
 		<< setw(20) << cost3 << " руб." << endl;
 
 	cout << string(60, '=') << endl;
+*/
+	
+	cout << "Здравствуйте, введите целое шестизначное число ниже";
+	int number = 0;
+	cin >> number;
 
+	if (number > 999999 || number < 100000) {
+		cout << "Число должно быть шестизначным";
+		return 1;
+	}
+	int firstpart = number / 1000;
+	int secondpart = number % 1000;
+	int sumFirst = (firstpart / 100) + ((firstpart / 10) % 10) + (firstpart % 10);
+	int sumSecond = (secondpart / 100) + ((secondpart / 10) % 10) + (secondpart % 10);
+
+	// Проверка на счастливое число  
+	if (sumFirst == sumSecond) {
+		cout << "Число счастливое!" << endl;
+	}
+	else {
+		cout << "Число не является счастливым." << endl;
+	}
+
+	string numberStr;
+
+
+	cout << "Введите четырехзначное число: ";
+	cin >> numberStr;
+
+
+	if (numberStr.length() != 4) {
+		cout << "Ошибка! Введите ровно четыре цифры." << endl;
+		return 1;
+	}
+
+
+	for (char c : numberStr) {
+		if (!isdigit(c)) {
+			cout << "Ошибка! Введены не только цифры." << endl;
+			return 1;
+		}
+	}
+
+
+	cout << "\nИсходное число: " << numberStr << endl;
+
+
+	swap(numberStr[0], numberStr[1]);
+	swap(numberStr[2], numberStr[3]);
+
+
+	cout << "Результат после замены: " << numberStr << endl;
+
+	int max = 0;
+	int number1;
+	cout << "Введите первое число: ";
+	cin >> number1;
+	number1 = max;
+	for (int i = 0; i < 6; i++) {
+		cout << "Введите число: ";
+		cin >> number1;
+		if (number1 > max) {
+			max = number1;
+		}
+		cout << "Максимальное число: " << max << endl;
+	}
 	
 	
 	
 	
 	
-	
-	
-	std::cout << "Hello fsdaf";
 return 0;
 }
